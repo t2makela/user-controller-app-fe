b1a1c968036e071ecc53c1267a687144eba62c5b
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2025-01-13 09:15:08 +0200

    Initial commit for project

---
+++ README.md
@@ -0,0 +1,7 @@
+# User controller application
+
+- Uses React, Typescript and Vite
+
+# Run application in development mode by typing to console:
+
+npm run dev
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>KÄYTTÄJIEN HALLINTAPANEELI</h1>
+    </>
+  );
+}
+
+export default App;
