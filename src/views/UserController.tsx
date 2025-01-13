80fd5dee9d4d7e913939c7bf97ab32e71f7f3828
Teemu Mäkelä <42958829+t2makela@users.noreply.github.com>
2025-01-13 10:27:33 +0200

    Appended data fetching for user data

--- src/views/UserController.tsx
+++ src/views/UserController.tsx
@@ -1,6 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { UserInfo } from "../types/userInfo";
 
 function UserController() {
+  const [userInfoData, setUserInfoData] = useState<
+    Array<UserInfo> | undefined
+  >();
+  const req = "https://jsonplaceholder.typicode.com/users";
+
+  async function getUserData() {
+    try {
+      const res = await fetch(req);
+
+      if (res.status === 200) {
+        const userData = await res.json();
+        setUserInfoData(userData);
+      }
+    } catch (err) {
+      console.log("Error in user data fetching: ", err);
+    }
+  }
+
+  useEffect(() => {
+    getUserData();
+  }, []);
+
   return (
     <div>
       <h1>KÄYTTÄJIEN HALLINTAPANEELI</h1>
